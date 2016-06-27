bda6f1a44595ee45fc4fdf8a342edc476ad0b24f
mjin <ming.jin@orbitz.com>
2016-06-27 08:52:41 -0500

    Created projects

---
+++ src/main/java/Rental.java
@@ -0,0 +1,39 @@
+class Rental {
+    private Movie _movie;
+    private int _daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        _movie = movie;
+        _daysRented = daysRented;
+    }
+
+    public int getDaysRented() {
+        return _daysRented;
+    }
+
+    public Movie getMovie() {
+        return _movie;
+    }
+
+    public double getThisAmount() {
+        double thisAmount = 0;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount = 2;
+                if (getDaysRented() > 2) {
+                    thisAmount += (getDaysRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount = getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount = 1.5;
+                if (getDaysRented() > 3) {
+                    thisAmount += (getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+        return thisAmount;
+    }
+}
---
+++ src/test/java/CustomerTest.java
@@ -0,0 +1,33 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CustomerTest {
+    private static final String STATEMENT = "Rental Record for Test\n" +
+            "\tAAA\t2.0\n" +
+            "\tBBB\t9.0\n" +
+            "\tCCC\t4.5\n" +
+            "Amount owed is 15.5\n" +
+            "You earned 4 frequent rental points";
+    @Test
+    public void test() {
+        Assert.assertEquals(STATEMENT, mockCustomer().statement());
+    }
+
+    private Customer mockCustomer() {
+        Customer customer = new Customer("Test");
+
+        customer.addRental(mockRental("AAA", 0, 1));
+        customer.addRental(mockRental("BBB", 1, 3));
+        customer.addRental(mockRental("CCC", 2, 5));
+
+        return customer;
+    }
+
+    private Rental mockRental(String title, int priceCode, int days) {
+        return new Rental(mockMovie(title, priceCode), days);
+    }
+
+    private Movie mockMovie(String title, int priceCode) {
+        return new Movie(title, priceCode);
+    }
+}
