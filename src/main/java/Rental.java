1f2fe77fa00ecfbb3878d2e4f7732cf7b636783d
mjin <ming.jin@orbitz.com>
2016-06-28 13:49:05 -0500

    After refactoring customer

--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -14,4 +14,35 @@ class Rental {
     public Movie getMovie() {
         return _movie;
     }
+
+    double getRentalCharge() {
+        double thisAmount = 0;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (getDaysRented() > 2) {
+                    thisAmount += (getDaysRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (getDaysRented() > 3) {
+                    thisAmount += (getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+        return thisAmount;
+    }
+
+    int getFrequentRenterPoints() {
+        int frequentRenterPoints = 1;
+        // add bonus for a two day new release rental
+        if (getMovie().getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > 1) {
+            frequentRenterPoints++;
+        }
+        return frequentRenterPoints;
+    }
 }
