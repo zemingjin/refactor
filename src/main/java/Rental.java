9dd404a64fb04759459809644cfd7ba0b4a292be
mjin <ming.jin@orbitz.com>
2016-07-06 11:31:11 -0500

    Refactored

--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -14,4 +14,36 @@ class Rental {
     public Movie getMovie() {
         return _movie;
     }
+
+    double getAmount() {
+        double thisAmount = 0;
+        //determine amounts for each line
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (getDaysRented() > 2) {
+                    thisAmount += (getDaysRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (getDaysRented() > 3) {
+                    thisAmount += (getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+        return thisAmount;
+    }
+
+    int getFrequentRenterPoints() {
+        int frequentRenterPoints = 1;
+
+        if (getMovie().getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > 1) {
+            frequentRenterPoints++;
+        }
+        return frequentRenterPoints;
+    }
 }
